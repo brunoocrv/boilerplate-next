9b06b943461853b2f39033b287206ed5f1898441
Bruno Carvalho <brn.carvalho@icloud.com>
2023-10-25 08:31:54 -0300

    chore: adding some test

---
+++ src/app/components/Main/index.tsx
@@ -0,0 +1,3 @@
+export function Main() {
+  return <h1 id="myHeading">teste</h1>
+}
---
+++ src/app/components/Main/test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react'
+import { Main } from '.'
+
+describe('<Main />', () => {
+  it('should render Main component', () => {
+    render(<Main />)
+
+    expect(screen.getByRole('heading', { name: /teste/ })).toBeInTheDocument()
+  })
+})
